8a7ec9beb877c88947c8587a9f2fcd0d5c1ef4f7
Tim DuBois <tim@neophilus.net>
2018-10-21 12:47:11 +0200

    Pull in manifest file. Closes #1

--- src/Main.elm
+++ src/Main.elm
@@ -5,9 +5,12 @@ import Browser.Navigation as Nav
 import Html exposing (Html, a, b, button, div, input, li, text, ul)
 import Html.Attributes exposing (href, type_, value)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder)
 import Partition exposing (Partition, largestDifference)
 import Task
-import Url
+import Url exposing (Url)
+import Url.Builder
 
 
 main : Program () Model Msg
@@ -28,16 +31,46 @@ main =
 
 type alias Model =
     { key : Nav.Key
-    , url : Url.Url
-    , sequence : List Float
+    , url : Url
     , left : List Int
     , right : List Int
+    , images : List Image
     }
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url [ 2.2598870056497176, 0.6665, 1.5, 2.094240837696335, 1.6835016835016836, 1.7777777777777777, 1.8050541516245486, 1.7777777777777777, 0.5625, 0.4975, 1.3540961408259986, 0.4135, 0.6875, 1.7777777777777777, 0.5625, 2.242152466367713, 0.544, 0.5845, 0.494, 1.4992503748125936 ] [] [], Cmd.none )
+    ( Model key url [] [] [], Http.send LoadManifest (Http.get manifest manifestDecoder) )
+
+
+manifest : String
+manifest =
+    Url.Builder.relative [ "..", "manifest.json" ] []
+
+
+type alias Image =
+    { thumbnail : String
+    , full : String
+    , aspectRatio : Float
+    , description : String
+    , locale : String
+    }
+
+
+manifestDecoder : Decoder (List Image)
+manifestDecoder =
+    Json.Decode.list <|
+        Json.Decode.map5 Image
+            (Json.Decode.field "small" Json.Decode.string)
+            (Json.Decode.field "big" Json.Decode.string)
+            (Json.Decode.field "aspect_ratio" Json.Decode.float)
+            (Json.Decode.field "desc" Json.Decode.string)
+            (Json.Decode.field "locale" Json.Decode.string)
+
+
+getRatios : List Image -> List Float
+getRatios images =
+    List.map .aspectRatio images
 
 
 
@@ -46,8 +79,9 @@ init flags url key =
 
 type Msg
     = LinkClicked Browser.UrlRequest
-    | UrlChanged Url.Url
+    | UrlChanged Url
     | GetPartition
+    | LoadManifest (Result Http.Error (List Image))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -69,9 +103,17 @@ update msg model =
         GetPartition ->
             let
                 ( left, right ) =
-                    largestDifference <| weights model.sequence
+                    largestDifference <| weights <| getRatios model.images
             in
             ( { model | left = left, right = right }, Cmd.none )
+
+        LoadManifest result ->
+            case result of
+                Ok imageList ->
+                    ( { model | images = imageList }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
 
 
 
@@ -96,10 +138,8 @@ view model =
         , ul []
             [ viewLink "/home"
             , viewLink "/profile"
-            , viewLink "/reviews/the-century-of-the-self"
-            , viewLink "/reviews/public-opinion"
-            , viewLink "/reviews/shah-of-shahs"
             ]
+        , div [] [ text (Debug.toString <| getRatios model.images) ]
         , button [ onClick GetPartition ] [ text "partition" ]
         , div [] [ text (Debug.toString model.left) ]
         , div [] [ text (Debug.toString model.right) ]
