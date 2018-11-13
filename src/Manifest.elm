b931d41fa4533db2ac17107cdfe1809d0c60d24d
Tim DuBois <tim@neophilus.net>
2018-11-13 14:47:08 +0100

    Functioning but not complete manifest module

--- src/Manifest.elm
+++ src/Manifest.elm
@@ -1,11 +1,14 @@
-module Manifest exposing (Image, Manifest)
+module Manifest exposing (Image, imageURL, locale, manifest, thumbURL)
+
+import List.Extra exposing (unconsLast)
+
+
 
 -- Would be good to autogenerate this file
 
 
 type alias Image =
     { file : String
-    , thumb : String
     , date : ( Year, Month )
     , location : Location
     , trip : Maybe Trip
@@ -14,19 +17,60 @@ type alias Image =
     }
 
 
+{-| TODO: There is an assumption here that `image.location.name` has the same value in
+the directory structure apart from an undescore replacing whitespace. Country cannot do that
+if we keep the d3-geo structure, since the names don't match.
+
+However, this may not actually be the case if we hard code values like we have here.
+So ultimately we could simplify the `countryToDirectory` to just `countryName` in the future perhaps.
+
+-}
 imagePath : Image -> String
 imagePath image =
     let
         ( year, month ) =
             image.date
+
+        info =
+            locationInformation image.location
     in
     String.join "/"
         [ "gallery"
         , String.fromInt year
         , monthToDirectory month
-        , countryToDirectory image.location.country
-        , String.replace " " "_" image.location.name
+        , countryToDirectory info.country
+        , String.replace " " "_" info.name
         ]
+
+
+imageURL : Image -> String
+imageURL image =
+    imagePath image ++ "/" ++ image.file
+
+
+thumbURL : Image -> String
+thumbURL image =
+    String.join "/" [ imagePath image, thumbnailFromFile image.file ]
+
+
+thumbnailFromFile : String -> String
+thumbnailFromFile file =
+    let
+        -- We split here and then join the name later to catch `file.name.jpg` conventions (if they are used)
+        splitFile =
+            unconsLast <| String.split "." file
+    in
+    case splitFile of
+        Just ( ext, splitName ) ->
+            let
+                name =
+                    String.join "." splitName
+            in
+            String.join "_small." [ name, ext ]
+
+        Nothing ->
+            -- Unsure if it's best to return the image or fail here. It'll look nicer with the image, but use more bandwidth
+            file
 
 
 
@@ -62,6 +106,175 @@ type Country
     | Thailand
     | Ukraine
     | Vietnam
+
+
+countryName : Country -> String
+countryName country =
+    case country of
+        Armenia ->
+            "Armenia"
+
+        Australia ->
+            "Australia"
+
+        Austria ->
+            "Austria"
+
+        CzechRepublic ->
+            "Czech Republic"
+
+        Germany ->
+            "Germany"
+
+        Denmark ->
+            "Denmark"
+
+        Estonia ->
+            "Estonia"
+
+        Finland ->
+            "Finland"
+
+        France ->
+            "France"
+
+        FaroeIslands ->
+            "Faroe Islands"
+
+        UnitedKingdom ->
+            "United Kingdom"
+
+        Greece ->
+            "Greece"
+
+        HongKong ->
+            "Hong Kong"
+
+        Hungary ->
+            "Hungary"
+
+        Iceland ->
+            "Iceland"
+
+        Japan ->
+            "Japan"
+
+        Latvia ->
+            "Latvia"
+
+        Netherlands ->
+            "Netherlands"
+
+        Norway ->
+            "Norway"
+
+        Poland ->
+            "Poland"
+
+        Qatar ->
+            "Qatar"
+
+        Russia ->
+            "Russia"
+
+        Singapore ->
+            "Singapore"
+
+        Slovakia ->
+            "Slovakia"
+
+        Sweden ->
+            "Sweden"
+
+        Thailand ->
+            "Thailand"
+
+        Ukraine ->
+            "Ukraine"
+
+        Vietnam ->
+            "Vietnam"
+
+
+{-| TODO: Historically we pull this out of world data. We can probably do that again.
+-}
+countryLocalName : Country -> Maybe String
+countryLocalName country =
+    case country of
+        Armenia ->
+            Just "Հայաստան"
+
+        Austria ->
+            Just "Österreich"
+
+        CzechRepublic ->
+            Just "Česká republika"
+
+        Germany ->
+            Just "Deutschland"
+
+        Denmark ->
+            Just "Danmark"
+
+        Estonia ->
+            Just "Eesti"
+
+        Finland ->
+            Just "Suomi"
+
+        FaroeIslands ->
+            Just "Føroyar"
+
+        Greece ->
+            Just "Ελλάδα"
+
+        HongKong ->
+            Just "香港"
+
+        Hungary ->
+            Just "Magyarország"
+
+        Iceland ->
+            Just "Ísland"
+
+        Japan ->
+            Just "日本"
+
+        Latvia ->
+            Just "Latvija"
+
+        Netherlands ->
+            Just "Nederland"
+
+        Norway ->
+            Just "Norge"
+
+        Poland ->
+            Just "Polska"
+
+        Qatar ->
+            Just "قطر"
+
+        Russia ->
+            Just "Росси́я"
+
+        Slovakia ->
+            Just "Slovensko"
+
+        Sweden ->
+            Just "Sverige"
+
+        Thailand ->
+            Just "ประเทศไทย"
+
+        Ukraine ->
+            Just "Україна"
+
+        Vietnam ->
+            Just "Việt Nam"
+
+        _ ->
+            Nothing
 
 
 countryToDirectory : Country -> String
@@ -169,38 +382,130 @@ type Location
     | Helsingborg
 
 
-
--- Country is listed here as well as Image. Can we drop the one in image (or here for that matter? Perhaps image is better)?
-
-
 type alias LocationInformation =
     { name : String
-    , localName : String
     , country : Country
     , coordinates : ( Float, Float )
     }
 
 
+locationLocalName : Location -> Maybe String
+locationLocalName location =
+    case location of
+        Gothenburg ->
+            Just "Göteborg"
+
+        Umea ->
+            Just "Umeå"
+
+        Are ->
+            Just "Åre"
+
+        Ostersund ->
+            Just "Östersund"
+
+        Trollhattan ->
+            Just "Trollhättan"
+
+        _ ->
+            Nothing
+
+
+{-| TODO: try to pull coordinates from an internet source automatically.
+Does latlong have an API?
+-}
 locationInformation : Location -> LocationInformation
 locationInformation location =
     case location of
         Gothenburg ->
             { name = "Gothenburg"
-            , localName = "Göteborg"
             , country = Sweden
             , coordinates = ( 11.97, 57.72 )
             }
 
-        _ ->
-            { name = "Tokyo"
-            , localName = "東京"
-            , country = Japan
-            , coordinates = ( 139.73, 35.71 )
+        Umea ->
+            { name = "Umea"
+            , country = Sweden
+            , coordinates = ( 20.26, 63.83 )
+            }
+
+        Are ->
+            { name = "Are"
+            , country = Sweden
+            , coordinates = ( 13.08, 63.4 )
+            }
+
+        Ostersund ->
+            { name = "Ostersund"
+            , country = Sweden
+            , coordinates = ( 14.64, 63.18 )
+            }
+
+        Trollhattan ->
+            { name = "Trollhattan"
+            , country = Sweden
+            , coordinates = ( 12.27, 58.28 )
+            }
+
+        Stockholm ->
+            { name = "Stockholm"
+            , country = Sweden
+            , coordinates = ( 18.07, 59.33 )
+            }
+
+        Revsund ->
+            { name = "Revsund"
+            , country = Sweden
+            , coordinates = ( 15.13, 62.9 )
+            }
+
+        Kinnekulle ->
+            { name = "Kinnekulle"
+            , country = Sweden
+            , coordinates = ( 13.38, 58.58 )
+            }
+
+        Lund ->
+            { name = "Lund"
+            , country = Sweden
+            , coordinates = ( 13.19, 55.71 )
+            }
+
+        Helsingborg ->
+            { name = "Helsingborg"
+            , country = Sweden
+            , coordinates = ( 12.69, 56.05 )
             }
 
 
+locale : Image -> String
+locale image =
+    let
+        info =
+            locationInformation image.location
 
--- TODO: locale can be generated too rather than storing it.
+        countryString =
+            countryName info.country
+    in
+    case countryLocalName info.country of
+        Just localCountry ->
+            case locationLocalName image.location of
+                Just localLocation ->
+                    localLocation ++ ", " ++ localCountry ++ " (" ++ info.name ++ ", " ++ countryString ++ ")"
+
+                Nothing ->
+                    info.name ++ ", " ++ countryString ++ " (" ++ localCountry ++ ")"
+
+        Nothing ->
+            case locationLocalName image.location of
+                Just localLocation ->
+                    localLocation ++ " (" ++ info.name ++ "), " ++ countryString
+
+                Nothing ->
+                    info.name ++ ", " ++ countryString
+
+
+
 -- TRIPS
 
 
@@ -229,14 +534,14 @@ tripInformation trip =
         SingaporeJapan2007 ->
             { name = "J07"
             , description = "Singapore/Japan 2007"
-            , locations = [ Melbourne, Singapore, Osaka, Himeji, Osaka, Hiroshima, Koyasan, Osaka, Tokyo, Kyoto, Osaka ]
+            , locations = [] --[ Melbourne, Singapore, Osaka, Himeji, Osaka, Hiroshima, Koyasan, Osaka, Tokyo, Kyoto, Osaka ]
             , dates = [ ( 2007, Dec ) ]
             }
 
         _ ->
             { name = "E12"
             , description = "Europe 2012"
-            , locations = [ Melbourne, Singapore, Frankfurt, Karlsruhe, Heidelberg, Munich, Vienna, Budapest, Prague, Riga, Copenhagen, Dronningmolle, Helsingor, Helsingborg, Copenhagen, Berlin, Paris, Singapore ]
+            , locations = [] --[ Melbourne, Singapore, Frankfurt, Karlsruhe, Heidelberg, Munich, Vienna, Budapest, Prague, Riga, Copenhagen, Dronningmolle, Helsingor, Helsingborg, Copenhagen, Berlin, Paris, Singapore ]
             , dates = [ ( 2012, Jun ), ( 2012, Jul ) ]
             }
 
@@ -307,4 +612,19 @@ monthToDirectory month =
                 Dec ->
                     12
     in
-    monthInt |> String.fromInt |> String.padLeft 2 '0'
+    monthInt
+        |> String.fromInt
+        |> String.padLeft 2
+            '0'
+
+
+
+-- MANIFEST
+
+
+manifest : List Image
+manifest =
+    [ Image "20151115_103822.jpg" ( 2015, Nov ) Gothenburg Nothing 1.778 "Slottsskogen has a few ducks."
+    , Image "DSC_1758.jpg" ( 2018, Jan ) Gothenburg Nothing 1.778 ""
+    , Image "20180102_141636.jpg" ( 2018, Jan ) Revsund Nothing 0.75 ""
+    ]
