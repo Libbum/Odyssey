b03e82389ee1231a59dca7e8b5bdf31a84706948
Tim DuBois <tim@neophilus.net>
2018-11-15 22:50:56 +0100

    Captures scrollbar width and sends it as a flag instead of using overflow-y scroll

--- src/Main.elm
+++ src/Main.elm
@@ -11,7 +11,7 @@ import Partition exposing (KPartition, greedyK)
 import Task
 
 
-main : Program () Model Msg
+main : Program Int Model Msg
 main =
     Browser.element
         { init = init
@@ -36,21 +36,21 @@ type alias Model =
     }
 
 
-initialModel : Model
-initialModel =
+initialModel : Int -> Model
+initialModel scrollWidth =
     { partition = []
     , images = manifest
     , sort = DateNewest
-    , filter = All
-    , viewportWidth = 0
+    , filter = ByLocation Melbourne
+    , viewportWidth = toFloat scrollWidth
     , locale = ""
     , zoom = Nothing
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init flags =
-    ( initialModel
+init : Int -> ( Model, Cmd Msg )
+init scrollWidth =
+    ( initialModel scrollWidth
     , getPartition
     )
 
@@ -84,11 +84,11 @@ update msg model =
                             getRatios <| filterImages model.filter model.images
 
                         rowsBest =
-                            optimalRowCount ratios vp.viewport.width vp.viewport.height
+                            optimalRowCount ratios (vp.viewport.width - model.viewportWidth) vp.viewport.height
                     in
                     ( { model
                         | partition = greedyK (weights ratios) rowsBest
-                        , viewportWidth = vp.viewport.width
+                        , viewportWidth = vp.viewport.width - model.viewportWidth
                       }
                     , Cmd.none
                     )
