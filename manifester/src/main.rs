6d1b5b621fb62643fd86d4d5b7ec7fb2e87e900c
Tim DuBois <tim@neophilus.net>
2019-01-10 23:09:41 +0100

    cities.json now constructed automatically by the manifester

--- manifester/src/main.rs
+++ manifester/src/main.rs
@@ -1,26 +1,35 @@
-#![recursion_limit="256"]
+#![recursion_limit = "256"]
 extern crate failure;
 extern crate globwalk;
+extern crate hashbrown;
 extern crate image;
 extern crate indicatif;
-extern crate hashbrown;
-#[macro_use] extern crate enum_derive;
-#[macro_use] extern crate macro_attr;
+extern crate reqwest;
+#[macro_use]
+extern crate enum_derive;
+#[macro_use]
+extern crate macro_attr;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 extern crate serde_yaml;
 
 use failure::Error;
+use hashbrown::HashMap;
 use image::FilterType::Lanczos3;
 use image::GenericImageView;
 use indicatif::{ProgressBar, ProgressStyle};
-use std::iter::FromIterator;
+use reqwest::header::USER_AGENT;
 use std::fs::File;
 use std::io::{Read, Write};
+use std::iter::FromIterator;
 use std::str::FromStr;
-use std::fmt;
-use hashbrown::HashMap;
+use std::{fmt, thread, time};
+
+static NOMINATIM_ENDPOINT: &str = "http://nominatim.openstreetmap.org";
+const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const NAME: &str = env!("CARGO_PKG_NAME");
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CountryCodes {
@@ -41,24 +50,26 @@ mod codes {
     use super::CountryCodeStruct;
     use hashbrown::HashMap;
 
-    use serde::ser::Serializer;
     use serde::de::{Deserialize, Deserializer};
+    use serde::ser::Serializer;
 
     pub fn serialize<S>(map: &HashMap<String, String>, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer
-        {
-            serializer.collect_seq(map.values())
-        }
+    where
+        S: Serializer,
+    {
+        serializer.collect_seq(map.values())
+    }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<HashMap<String, String>, D::Error>
-        where D: Deserializer<'de>
-        {
-            let mut map = HashMap::new();
-            for item in Vec::<CountryCodeStruct>::deserialize(deserializer)? {
-                map.insert(item.name, item.alpha3);
-            }
-            Ok(map)
+    where
+        D: Deserializer<'de>,
+    {
+        let mut map = HashMap::new();
+        for item in Vec::<CountryCodeStruct>::deserialize(deserializer)? {
+            map.insert(item.name, item.alpha3);
         }
+        Ok(map)
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -84,7 +95,7 @@ struct Properties {
     country: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Geometry {
     #[serde(rename = "type")]
     type_: String,
@@ -124,7 +135,10 @@ impl FromStr for Month {
             "10" => Ok(Month::Oct),
             "11" => Ok(Month::Nov),
             "12" => Ok(Month::Dec),
-            err => Err(failure::err_msg(format!("{} makes no sense to be a month.", err))),
+            err => Err(failure::err_msg(format!(
+                "{} makes no sense to be a month.",
+                err
+            ))),
         }
     }
 }
@@ -272,6 +286,39 @@ impl fmt::Display for Location {
     }
 }
 
+#[derive(Deserialize, Debug)]
+struct LatLon {
+    lat: String,
+    lon: String,
+}
+
+fn get_query_string(params: Vec<(&str, &str)>) -> String {
+    let pairs: Vec<String> = params
+        .into_iter()
+        .map(|(k, v)| format!("{}={}", k, v))
+        .collect();
+    pairs.join("&")
+}
+
+fn search(place_name: &str) -> Result<LatLon, Error> {
+    let params = vec![("format", "jsonv2"), ("q", place_name), ("limit", "1")];
+    let query_string = get_query_string(params);
+    let url = format!("{}/search?{}", NOMINATIM_ENDPOINT, query_string);
+    let client = reqwest::Client::new();
+    let mut res = client
+        .get(&url)
+        .header(USER_AGENT, format!("{} v{} - {}", NAME, VERSION, AUTHORS))
+        .send()?;
+    let mut results = res.json::<Vec<LatLon>>()?;
+    results.reverse();
+    let first = results.pop().ok_or_else(|| {
+        failure::err_msg(format!(
+            "Search for {} did not find coordinates",
+            place_name
+        ))
+    })?;
+    Ok(first)
+}
 
 fn name_from_string(from: &str) -> String {
     let mut name: Vec<char> = Vec::new();
@@ -289,37 +336,42 @@ fn main() -> Result<(), Error> {
     let walker = globwalk::GlobWalkerBuilder::from_patterns(
         "../dist/gallery/",
         &["*.{png,jpg,jpeg,PNG,JPG,JPEG}", "!*_small*", "!*_blur*"],
-        )
-        .follow_links(true)
-        .build()?
-        .into_iter()
-        .filter_map(Result::ok);
+    )
+    .follow_links(true)
+    .build()?
+    .into_iter()
+    .filter_map(Result::ok);
 
     // This is a little annoying, we can't size_hint this iterator, so we must count it.
     let progcount = globwalk::GlobWalkerBuilder::from_patterns(
         "../dist/gallery/",
         &["*.{png,jpg,jpeg,PNG,JPG,JPEG}", "!*_small*", "!*_blur*"],
-        )
-        .follow_links(true)
-        .build()?
-        .into_iter()
-        .filter_map(Result::ok)
-        .count() as u64;
+    )
+    .follow_links(true)
+    .build()?
+    .into_iter()
+    .filter_map(Result::ok)
+    .count() as u64;
     let bar = ProgressBar::new(progcount);
-    bar.set_style(ProgressStyle::default_bar()
-                  .template("[{elapsed_precise}] {bar:25.cyan/blue} {pos:>5}/{len:5} {msg}"));
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:25.cyan/blue} {pos:>5}/{len:5} {msg}"),
+    );
 
     let cca3_file = File::open("cca3.json")?;
-    let cca3_read: CountryCodes  = serde_json::from_reader(cca3_file)?;
+    let cca3_read: CountryCodes = serde_json::from_reader(cca3_file)?;
     let cca3 = &cca3_read.codes;
 
     let config_file = File::open("odyssey.yaml")?;
     let config: Config = serde_yaml::from_reader(config_file)?;
     let mut features: Vec<Feature> = Vec::new();
+    let pause = time::Duration::from_secs(1);
     for (country_string, locations) in &config {
         //let country = country_string.parse::<Country>()?;
         let country_name = name_from_string(&country_string);
-        let country_code = cca3.get(&country_name).ok_or_else(|| failure::err_msg(format!("{} does not exist in cca3.json", country_name)))?;
+        let country_code = cca3.get(&country_name).ok_or_else(|| {
+            failure::err_msg(format!("{} does not exist in cca3.json", country_name))
+        })?;
 
         //let mut country_local_name = None;
         //if let Some(local) = locations.get("local") {
@@ -327,29 +379,52 @@ fn main() -> Result<(), Error> {
         //}
         //        println!("{}: {}, {:?}, {}", country.to_string(), country_name, country_local_name, country_code);
 
-        for (location_string, local_name) in locations.iter().filter(|(l,_)| *l != "local") {
+        for (location_string, local_name) in locations.iter().filter(|(l, _)| *l != "local") {
             let location = location_string.parse::<Location>()?;
             let mut location_name = name_from_string(&location_string);
             if location_name.ends_with("City") && location != Location::HoChiMinhCity {
-                location_name.truncate(location_name.len()-5);
+                location_name.truncate(location_name.len() - 5);
             }
+            let coords = search(&format!("{}, {}", location_name, country_name))?;
+            let properties = Properties {
+                name: location_name,
+                localname: local_name.to_owned(),
+                country: country_code.to_string(),
+            };
+            let geometry = Geometry {
+                type_: "Point".to_string(),
+                coordinates: vec![coords.lon.parse::<f32>()?, coords.lat.parse::<f32>()?],
+            };
 
-            let properties = Properties { name: location_name, localname: local_name.to_owned(), country: country_code.to_string() };
-            let geometry = Geometry { type_: "Point".to_string(), coordinates: Vec::new() }; //TODO: Pull coordinates
+            println!("{} {:?}", location.to_string(), geometry.coordinates);
+            features.push(Feature {
+                type_: "Feature".to_string(),
+                properties,
+                geometry,
+            });
+            thread::sleep(pause); //We can't hammer the Nominatim server.
 
-            features.push(Feature { type_: "Feature".to_string(), properties, geometry });
             //           println!("    {}: {}, {:?}", location, location_name, local_name);
         }
     }
 
-    let cities = FeatureCollection { type_: "FeatureCollection".to_string(), features };
+    let cities = FeatureCollection {
+        type_: "FeatureCollection".to_string(),
+        features,
+    };
     let buffer = File::create("cities.json")?;
     serde_json::to_writer(&buffer, &cities)?;
 
     let mut manifest = File::create("Manifest.elm")?;
 
     for file in bar.wrap_iter(walker) {
-        bar.set_message(&file.path().strip_prefix("../dist/gallery/")?.to_str().unwrap_or_default());
+        bar.set_message(
+            &file
+                .path()
+                .strip_prefix("../dist/gallery/")?
+                .to_str()
+                .unwrap_or_default(),
+        );
         // Open image and grab its dimensions.
         let img = image::open(&file.path())?;
         let (width, height) = img.dimensions();
@@ -369,7 +444,9 @@ fn main() -> Result<(), Error> {
         let thumbnail = format!("{}_small.{}", stem, ext);
         let blur = format!("{}_blur.{}", stem, ext);
         let thumb_width = if ratio < 3.0 { 500 } else { 900 };
-        if !file.path().with_file_name(&thumbnail).exists() && !file.path().with_file_name(&blur).exists()  {
+        if !file.path().with_file_name(&thumbnail).exists()
+            && !file.path().with_file_name(&blur).exists()
+        {
             let thumb = img.resize(thumb_width, 500, Lanczos3);
             thumb.save(file.path().with_file_name(thumbnail))?;
             thumb.blur(30.0).save(file.path().with_file_name(blur))?;
@@ -377,13 +454,14 @@ fn main() -> Result<(), Error> {
             img.resize(thumb_width, 500, Lanczos3)
                 .save(file.path().with_file_name(thumbnail))?;
         } else {
-            img.resize(thumb_width, 500, Lanczos3).blur(30.0)
+            img.resize(thumb_width, 500, Lanczos3)
+                .blur(30.0)
                 .save(file.path().with_file_name(blur))?;
         }
 
         // Get image decription if it exists, create file if not.
         let mut description = String::new();
-        let _ =File::open(file.path().with_extension("desc"))
+        let _ = File::open(file.path().with_extension("desc"))
             .or_else(|_| File::create(file.path().with_extension("desc")))
             .and_then(|mut f| f.read_to_string(&mut description));
 
@@ -410,7 +488,16 @@ fn main() -> Result<(), Error> {
             .and_then(|p| p.to_str())
             .ok_or_else(|| failure::err_msg("Year unwrap issue."))?;
 
-        write!(manifest, ",Image \"{}\" (Date {} {:?}) {:?} {:.3} \"{}\"\n", name, year, month, location, ratio, description.trim())?;
+        write!(
+            manifest,
+            ",Image \"{}\" (Date {} {:?}) {:?} {:.3} \"{}\"\n",
+            name,
+            year,
+            month,
+            location,
+            ratio,
+            description.trim()
+        )?;
     }
     bar.finish();
 
